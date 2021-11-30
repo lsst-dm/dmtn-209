8bd1306b21a504426164d3bb554b2a24375a1c12
William O'Mullane <womullan@lsst.org>
2021-11-30 16:53:32 -0700

    keep mor eout of zip

--- mktar.sh
+++ mktar.sh
@@ -1,1 +1,1 @@
-tar --exclude=*out --exclude=.git* --exclude=README.rst --exclude='*txt' --exclude=.github --exclude=mktar.sh --exclude lsst-texmf -czf ../O3-002-v1.tar.gz .
+tar --exclude=*slides* --exclude=*py --exclude=*out --exclude=__* --exclude=.??* --exclude=README.rst --exclude='*txt' --exclude=.github --exclude=mktar.sh --exclude lsst-texmf -czf ../O3-002-v1.tar.gz .
